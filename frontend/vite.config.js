cd70f3155334729c59fd73cb69312f69c7f376c5
JackScofield <apuaimanlian@gmail.com>
2025-04-22 23:55:20 -0500

    change base

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/zeyuan-blog/",
+  base: "/jackscofield.github.io/",
   plugins: [react()],
 })
